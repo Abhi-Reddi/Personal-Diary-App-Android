ce866cd7b07f6630441c758445a68acdc7ad6342
Tharun <tharunreddydendi2000@gmail.com>
2024-10-21 05:05:23 -0400

    Added a button to save the notes along with the date:

--- DiaryNotes/app/src/main/java/com/example/diarynotes/NoteActivity.java
+++ DiaryNotes/app/src/main/java/com/example/diarynotes/NoteActivity.java
@@ -1,29 +1,27 @@
 package com.example.diarynotes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class NoteActivity extends AppCompatActivity {
 
     private TextView noteDateTextView;
     private EditText noteEditText;
+    private Button saveNoteButton;
+    private String selectedDate;
+
+    // Firebase database reference
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference notesDatabaseReference;
 
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note);
-        noteDateTextView = findViewById(R.id.noteDateTextView);
-        noteEditText = findViewById(R.id.noteEditText);
-        String selectedDate = getIntent().getStringExtra("selectedDate");
-        if (selectedDate != null) {
-            noteDateTextView.setText("Date: " + selectedDate);
-        }
-
-    }
 }
