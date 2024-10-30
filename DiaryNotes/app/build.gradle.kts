05f5bdaae14f07c7b1a03a283b88ebf4b9417013
Tharun <tharunreddydendi2000@gmail.com>
2024-10-30 11:11:05 -0400

    added dependencies to the build.gradle

--- DiaryNotes/app/build.gradle.kts
+++ DiaryNotes/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -33,6 +34,8 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // For JSON parsing
 
     implementation(libs.appcompat)
     implementation(libs.material)
